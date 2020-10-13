5c9ae50a693e782fa5895388074dc910d5f57a67
Igor Clark <igor@artrabbit.com>
2020-10-13 15:43:38 +0100

    updating script runner data paths

--- scripts/run_group.py
+++ scripts/run_group.py
@@ -40,8 +40,9 @@ mda_vectors = {
     '6': biannual
 }
 
-BetFilePath = f"data/Trachoma200/InputBet_group{group}.csv"  
-InSimFilePath = f"data/Trachoma200/OutputVals_group{group}.p"  
+FilePathRoot = f"data/merged"
+BetFilePath = f"{FilePathRoot}/InputBet_group{group}.csv"  
+InSimFilePath = f"{FilePathRoot}/OutputVals_group{group}.p"  
 
 if not pathlib.Path( BetFilePath ).is_file():
     print( f"Input beta file {BetFilePath} doesn't exist, exiting." )
@@ -62,11 +63,11 @@ for MDA_Cov in [ 0.6, 0.7, 0.8, 0.9 ]:
         for mda_list in mda_lists:
 
             # mkdir -p group dirs
-            output_dir = f"data/Trachoma200/output/group-{group}/coverage-{MDA_Cov}/mdatype-{mda_type}"
-            pathlib.Path( output_dir ).mkdir( parents = True, exist_ok = True )
+            output_dir = f"{FilePathRoot}/output/group-{group}/coverage-{MDA_Cov}/mdatype-{mda_type}"
+#            pathlib.Path( output_dir ).mkdir( parents = True, exist_ok = True )
 
-            input_dir = f"data/Trachoma200/input/group-{group}/coverage-{MDA_Cov}/mdatype-{mda_type}"
-            pathlib.Path( input_dir ).mkdir( parents = True, exist_ok = True )
+            input_dir = f"{FilePathRoot}/input/group-{group}/coverage-{MDA_Cov}/mdatype-{mda_type}"
+#            pathlib.Path( input_dir ).mkdir( parents = True, exist_ok = True )
 
             mda_list_string = '-'.join( [ str( x ) for x in mda_list ] )
             file_name_root = f"{group}-{MDA_Cov}-{mda_type}-{mda_list_string}"
@@ -85,13 +86,14 @@ for MDA_Cov in [ 0.6, 0.7, 0.8, 0.9 ]:
             } ] )
 
             # write Input MDA to file
-            df.to_csv( MDAFilePath, index=None )
+#            df.to_csv( MDAFilePath, index=None )
 
             # output CSV file path
             csv_file_name = f"{file_name_root}.csv"
             PrevFilePath = f"{output_dir}/{csv_file_name}"
 
             print( f"=== Running:\n\tMDA_Cov {MDA_Cov}\n\tvector {mda_type}\n\tinput {MDAFilePath}\n\toutput {PrevFilePath}\n" )
+            continue
 
             # run the simulation
             Trachoma_Simulation(
