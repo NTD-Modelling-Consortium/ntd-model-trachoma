c595dc5b7b01f62eee3abb96d88784e9db0098b8
Igor Clark <igor@artrabbit.com>
2020-10-16 09:32:16 +0100

    remove 'continue' which stops tests after printout

--- scripts/run_group.py
+++ scripts/run_group.py
@@ -1,23 +1,3 @@
-'''
-Panayiota has grouped the IUs into groups so we need to do less runs. To run each group we would need to:
-- take the InputBet_groupx file which contains the 200 beta parameters and pass it as BetFilePath
-- take the OutputVals_groupx file which contains the population and pass it as InSimFilePath
-- the PrevFilePath is just where the model outputs the runs so this should be where we define the naming etc for the output
-- SaveOutput can be false and OutSimFilePath can be none
-
-This is the basic setup for a group of ius and the foundation for our various scenarios. For the scenarios we want to have results for:
-- MDAs on or off every 6 months for 10 years (2020 - 2030)
-- and we would need to run all of these scenarios for rho 0, 0.1, 0.2, 0.3 
-(Rho is currently a fixed value in https://github.com/ArtRabbitStudio/ntd-model-trachoma/blob/master/trachoma/trachoma_simulations.py on line 161, we need to write something so we can pass this in when we run our scenarios)
-
-These are the group scenarios we need, we then have the mapping from groups to actual IUs which is in IUcodeTrachomaGroupsEthiopia.csv in Panaiyotas files. The relevant columns are IUcode and Group.
-That’s all the scenarios we need for the simulator output.
-
-For the setup page where I want to display historical prevalence data we’d need to take the OutputPrev_groupx file take the prevalence for each year (eg. 02-2014) and find the mean of the 200 runs. I just need a row with prevalence values and have all the IUs added together into the country-level.csv file (example in public/diseases/trachoma/country-level.csv).
-
-We should also think about that she’s just given us Ethiopia and can give us the other countries as well, so what we write should be able to take another set of files, or once we got it going and everything seems to be good, we ask her to produce a full set.
-'''
-
 import itertools
 import pandas as pd
 import json
@@ -93,7 +73,6 @@ for MDA_Cov in [ 0.6, 0.7, 0.8, 0.9 ]:
             PrevFilePath = f"{output_dir}/{csv_file_name}"
 
             print( f"=== Running:\n\tMDA_Cov {MDA_Cov}\n\tvector {mda_type}\n\tinput {MDAFilePath}\n\toutput {PrevFilePath}\n" )
-            continue
 
             # run the simulation
             Trachoma_Simulation(
