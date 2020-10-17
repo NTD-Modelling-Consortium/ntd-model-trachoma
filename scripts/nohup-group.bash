de3a36c88de91c6dabc5c104b225410758cecf30
Igor Clark <igor@artrabbit.com>
2020-10-17 10:32:27 +0100

    specify group number in nohup output file names

--- scripts/nohup-group.bash
+++ scripts/nohup-group.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nohup pipenv run python scripts/run_group.py $1 > run_group.out 2> run_group.err < /dev/null &
+nohup pipenv run python scripts/run_group.py "$1" > "run_group_$1.out" 2> "run_group_$1.err" < /dev/null &
