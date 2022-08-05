20ce63c52bf8972f348cc7ddc7a8333ce8b3d9a4
Igor Clark <igor@igorclark.net>
2022-08-05 11:22:26 +0100

    extract model runner scripts for separate repo

--- scripts/nohup-group.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-nohup pipenv run python scripts/run_group.py "$1" > "run_group_$1.out" 2> "run_group_$1.err" < /dev/null &
-echo "--- started background group run for group $1 on host $( hostname )"
--- scripts/run_all_groups.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=data/merged
-
-for group in $( ls -1 ${DIR}/InputBet_group*.csv | sed 's/.*_group\([0-9]*\)\.csv/\1/g' ) ; do
-    [[ -f "${DIR}/OutputVals_group${group}.p" ]] && python run_group.py ${group} || echo "OutputVals file not found for group $group"
-done
