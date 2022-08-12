cbbb725eeb40557af74f0bb49f1e872119efa11a
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-08-12 15:26:49 +0100

    Test output values according to reference output (#4)
    * Test check that output values are consistent across versions
    
    * Update CI workflow
    
    * use only one beta value
    
    Potential bug when using more - using only one for now, see #3
    
    * Fix location of test data
    
    * Switch back to triggering CI on main branch]

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,37 @@
 from trachoma import Trachoma_Simulation
+import numpy.testing
 
 def main():
     Trachoma_Simulation("tests/data/beta_values.csv",
                         "tests/data/mda_input_2008_2017.csv",
                         "prevalence_job.csv",
                         "infection_job.csv",
-                        SaveOutput = False,
-                        OutSimFilePath = None,
+                        SaveOutput = True,
+                        OutSimFilePath = "results/out.p",
                         InSimFilePath = None,
                         logger = None)
 
 if __name__ == "__main__":
+    import pickle
+
     main()
+
+    with open("results/out.p", "rb") as f:
+        sim_outputs = pickle.load(f)
+
+    for i, vals in enumerate(sim_outputs):
+        expected_final_pop_state = numpy.loadtxt(f'tests/reference_output/final_pop_state_{i}.txt', delimiter=',').reshape(1000, 3)
+        expected_yearly_infect_prev = numpy.loadtxt(f'tests/reference_output/yearly_infect_prev_{i}.txt', delimiter=',').reshape(2704, 2)
+
+        expected = {}
+        expected['IndI'] = expected_final_pop_state[:,0]
+        expected['IndD'] = expected_final_pop_state[:,1]
+        expected['No_Inf'] = expected_final_pop_state[:,2]
+        expected['True_Prev_Disease_children_1_9'] = expected_yearly_infect_prev[:,0]
+        expected['True_Infections_Disease_children_1_9'] = expected_yearly_infect_prev[:,1]
+
+        numpy.testing.assert_array_equal(vals['IndI'], expected['IndI'])
+        numpy.testing.assert_array_equal(vals['IndD'], expected['IndD'])
+        numpy.testing.assert_array_equal(vals['No_Inf'], expected['No_Inf'])
+        numpy.testing.assert_allclose(vals['True_Prev_Disease_children_1_9'], expected['True_Prev_Disease_children_1_9'], atol=5.e-7, rtol=2.e-4)
+        numpy.testing.assert_allclose(vals['True_Infections_Disease_children_1_9'], expected['True_Infections_Disease_children_1_9'], atol=5.e-7, rtol=2.e-4)
