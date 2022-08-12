ff360f656b1ff09bba2389c3e7d6967f401635d1
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-08-12 15:02:35 +0100

    Test check that output values are consistent across versions

---
+++ tests/generate_test_data.py
@@ -0,0 +1,32 @@
+from trachoma import Trachoma_Simulation
+import numpy.testing
+import pdb
+
+def main():
+    Trachoma_Simulation("data/beta_values.csv",
+                        "data/mda_input_2008_2017.csv",
+                        "prevalence_job.csv",
+                        "infection_job.csv",
+                        SaveOutput = True,
+                        OutSimFilePath = "results/out.p",
+                        InSimFilePath = None,
+                        logger = None)
+
+if __name__ == "__main__":
+    import pickle
+    main()
+    with open("results/out.p", "rb") as f:
+        sim_outputs = pickle.load(f)
+    final_pop_state = numpy.empty((1000, 3))
+    yearly_infect_prev = numpy.empty((2704, 2))
+    for i, vals in enumerate(sim_outputs):
+        final_pop_state[:,0] = vals['IndI']
+        final_pop_state[:,1] = vals['IndD']
+        final_pop_state[:,2] = vals['No_Inf']
+        yearly_infect_prev[:,0] = vals['True_Prev_Disease_children_1_9']
+        yearly_infect_prev[:,1] = vals['True_Infections_Disease_children_1_9']
+        pdb.set_trace()
+        with open(f"results/final_pop_state_{i}.txt", "w") as f:
+            final_pop_state.tofile(f, sep=',', format='%i')
+        with open(f"results/yearly_infect_prev_{i}.txt", "w") as f:
+            yearly_infect_prev.tofile(f, sep=',', format='%f')
