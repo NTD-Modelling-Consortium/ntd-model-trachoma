8b4e98061aae0ded136b071e01ded9cfff4635f5
mattg3004 <matthewgraham3004@gmail.com>
2023-03-15 18:31:14 +0800

    remove author ref

--- simple_example.py
+++ simple_example.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 15 18:16:41 2023
-
-@author: matthew
-"""
-
 from trachoma.trachoma_functions import *
 import multiprocessing
 import time
