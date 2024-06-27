c9aff67e61e9593f777a5aac496c12490d989198
jeannielynnmoulton <jeannielynnmoulton@gmail.com>
2024-06-27 15:31:01 +0100

    Use pytest to run tests.

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setuptools.setup(
     long_description='Set MDA times, initialise infection, and simulate transmission / MDA interventions.',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=['numpy', 'pandas', 'joblib', 'google-cloud-storage', 'matplotlib', 'openpyxl'],
+    install_requires=['numpy', 'pandas', 'joblib', 'google-cloud-storage', 'matplotlib', 'openpyxl', 'pytest'],
     include_package_data=True
 )
