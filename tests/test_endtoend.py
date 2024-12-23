cc3bf0e6a9cd55287192c703c01d9445a5c6f03e
mattg3004 <matthewgraham3004@gmail.com>
2024-12-23 12:05:18 +0800

    update end to end test

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -199,7 +199,7 @@ class EndToEndTest(unittest.TestCase):
                                                 VaccData = VaccData,
                                                 outputTimes= outputTimes,
                                                 index = i,
-                                                doSurvey = True, 
+                                                doSurvey = False, 
                                                 doIHMEOutput = True, 
                                                 # We use a fresh state for each simulation
                                                 numpy_state=numpy_states[i]) for i in range(numSims))
